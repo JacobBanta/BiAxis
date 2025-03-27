9f95671775e57eb89bdd82616ca673b538784686
Jacob Banta <jacob@banta.email>
2025-03-27 08:38:36 -0400

    add getRegistry()

--- engine.zig
+++ engine.zig
@@ -5,3 +5,11 @@ pub fn getZon(b: *std.Build, obj: anytype) []const u8 {
     std.zon.stringify.serialize(obj, .{}, buf.writer()) catch unreachable;
     return buf.items;
 }
+
+pub fn getRegistry(allocator: std.Allocator, files: []const []const u8) ![]const u8 {
+    var buf = std.ArrayList(u8).init(allocator);
+    for (files) |file| {
+        try buf.writer().print("pub const @\"{s}\" = @import(\"{s}\");\n", .{ file, file });
+    }
+    return buf.items;
+}
