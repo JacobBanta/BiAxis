961f23f5aa150bf17e704be6506d126cdd86bff7
Jacob Banta <jacob@banta.email>
2025-03-27 10:28:17 -0400

    add Param and ParamList

--- engine.zig
+++ engine.zig
@@ -6,10 +6,34 @@ pub fn getZon(b: *std.Build, obj: anytype) []const u8 {
     return buf.items;
 }
 
-pub fn getRegistry(allocator: std.Allocator, files: []const []const u8) ![]const u8 {
+pub fn getRegistry(allocator: std.Allocator, files: []const []const u8) []const u8 {
     var buf = std.ArrayList(u8).init(allocator);
     for (files) |file| {
-        try buf.writer().print("pub const @\"{s}\" = @import(\"{s}\");\n", .{ file, file });
+        buf.writer().print("pub const @\"{s}\" = @import(\"{s}\");\n", .{ file, file }) catch unreachable;
     }
     return buf.items;
+}
+
+pub fn Param(T: type, name: [:0]const u8, value: T) type {
+    return @Type(.{ .Struct = .{
+        .layout = .auto,
+        .is_tuple = false,
+        .fields = &[_]std.builtin.Type.StructField{.{
+            .alignment = @alignOf(T),
+            .is_comptime = false,
+            .type = T,
+            .default_value = &value,
+            .name = name,
+        }},
+        .decls = &[_]std.builtin.Type.Declaration{},
+    } });
+}
+
+pub fn ParamList(params: anytype) type {
+    if (@TypeOf(params) == void) return ParamList(.{});
+    var ret: std.builtin.Type = @typeInfo(struct {});
+    inline for (params) |P| {
+        ret.Struct.fields = ret.Struct.fields ++ @typeInfo(P).Struct.fields;
+    }
+    return @Type(ret);
 }
