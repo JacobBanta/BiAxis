3eca0cc7954d288404b224efa27daaae155aff1f
Jacob Banta <jacob@banta.email>
2025-03-27 10:34:36 -0400

    add scene data IR

--- engine.zig
+++ engine.zig
@@ -1,20 +1,19 @@
 const std = @import("std");
 
-pub fn getZon(b: *std.Build, obj: anytype) []const u8 {
-    var buf = std.ArrayList(u8).init(b.allocator);
-    std.zon.stringify.serialize(obj, .{}, buf.writer()) catch unreachable;
-    return buf.items;
-}
-
-pub fn getRegistry(allocator: std.Allocator, files: []const []const u8) []const u8 {
+pub fn getRegistry(allocator: std.Allocator, files: []const []const u8) []const u8 { //{{{
     var buf = std.ArrayList(u8).init(allocator);
     for (files) |file| {
         buf.writer().print("pub const @\"{s}\" = @import(\"{s}\");\n", .{ file, file }) catch unreachable;
     }
     return buf.items;
-}
+} //}}}
+pub fn getZon(b: *std.Build, obj: anytype) []const u8 { //{{{
+    var buf = std.ArrayList(u8).init(b.allocator);
+    std.zon.stringify.serialize(obj, .{}, buf.writer()) catch unreachable;
+    return buf.items;
+} //}}}
 
-pub fn Param(T: type, name: [:0]const u8, value: T) type {
+pub fn Param(T: type, name: [:0]const u8, value: T) type { //{{{
     return @Type(.{ .Struct = .{
         .layout = .auto,
         .is_tuple = false,
@@ -36,4 +35,19 @@ pub fn ParamList(params: anytype) type {
         ret.Struct.fields = ret.Struct.fields ++ @typeInfo(P).Struct.fields;
     }
     return @Type(ret);
-}
+} //}}}
+
+pub const Scene = struct { //{{{
+    entities: []const Entity = &[_]Entity{},
+};
+
+pub const Entity = struct {
+    scripts: []const Script = &[_]Script{},
+};
+
+pub const Script = union(enum) {
+    script: struct {
+        path: []const u8,
+        data: []const u8, //ZON data
+    },
+}; //}}}
