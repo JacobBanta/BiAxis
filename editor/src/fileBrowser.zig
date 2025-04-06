8f02355e3112b2f6fb5af42fe98430390bae06f2
Jacob Banta <jacob@banta.email>
2025-04-05 21:25:38 -0400

    fileBrowser improvments

--- editor/src/fileBrowser.zig
+++ editor/src/fileBrowser.zig
@@ -4,7 +4,7 @@ const SIZE = 150;
 files: std.BoundedArray([:0]const u8, 30) = undefined,
 filePaths: std.BoundedArray([:0]const u8, 30) = undefined,
 fileLayout: i32 = 0,
-path: [:0]const u8 = "",
+path: [:0]const u8 = "src",
 fileSelected: ?i32 = null,
 
 size: struct {
@@ -39,16 +39,30 @@ pub fn leftClickEvent(self: *@This(), x: i32, y: i32) void {
             self.fileSelected = fileSelected;
         }
     } else {
+        const path = self.getPath(x);
         if (self.fileSelected) |f| {
-            if (f == fileSelected) {
-                // TODO: buncha math
-                // this is for selecting the folder in the top display
+            if (f == path) {
+                var index: i32 = 0;
+                var chars: i32 = 0;
+                var it = std.mem.splitSequence(u8, self.path, std.fs.path.sep_str);
+                while (it.next()) |i| {
+                    if (i.len == 0) continue;
+                    index -= 1;
+                    chars += @intCast(i.len + 1);
+                    if (index == path) break;
+                }
+                for (self.filePaths.slice(), 0..) |f2, i| {
+                    if (std.mem.eql(u8, f2, self.path[0..@intCast(chars - 1)])) {
+                        self.path = self.filePaths.get(i);
+                    }
+                }
+
                 self.fileSelected = null;
             } else {
-                self.fileSelected = fileSelected;
+                self.fileSelected = path;
             }
         } else {
-            self.fileSelected = fileSelected;
+            self.fileSelected = path;
         }
     }
 }
@@ -65,6 +79,23 @@ pub fn getFile(self: @This(), file: i32) [:0]const u8 {
         index += 1;
     }
     return "";
+}
+
+pub fn getPath(self: @This(), x: i32) ?i32 {
+    if (x < 8) return null;
+    var newX: i32 = 8;
+    var index: i32 = 0;
+    var it = std.mem.splitSequence(u8, self.path, std.fs.path.sep_str);
+    while (it.next()) |i| {
+        if (i.len == 0) continue;
+        index -= 1;
+        const text = std.heap.page_allocator.dupeZ(u8, i) catch unreachable;
+        defer std.heap.page_allocator.free(text);
+        newX += rl.measureText(text, 20) + 15;
+        if (x < newX) return index;
+    }
+
+    return null;
 }
 
 pub fn resizeEvent(self: *@This(), w: i32, h: i32) void {
@@ -89,8 +120,27 @@ pub fn render_(self: @This(), offsetX: i32, offsetY: i32) void {
                 rl.drawRectangleLines(1 + offsetX + @mod(index, self.fileLayout) * SIZE, 1 + offsetY + topTextSize + @divFloor(index, self.fileLayout) * SIZE, SIZE - 2, SIZE - 2, rl.Color.orange);
             }
         }
-        const xSize = rl.measureText(f[self.path.len..], 20);
-        rl.drawText(f[if (self.path.len == 0) 0 else self.path.len + 1..], offsetX + @mod(index, self.fileLayout) * SIZE + 3 + @divFloor(SIZE - xSize, 2), offsetY + topTextSize + 3 + @divFloor(index, self.fileLayout) * SIZE, 20, rl.Color.white);
+        var xSize = rl.measureText(f[if (self.path.len == 0) 0 else (self.path.len + 1)..], 20);
+
+        if (xSize > SIZE) {
+            const text = std.heap.page_allocator.dupeZ(u8, f[if (self.path.len == 0) 0 else self.path.len + 1..]) catch unreachable;
+            defer std.heap.page_allocator.free(text);
+            var index2: usize = 1;
+            text[text.len - 2] = '.';
+            text[text.len - 3] = '.';
+            text[text.len + 0 - index2] = 0;
+            while (true) {
+                text[text.len - 3 - index2] = '.';
+                text[text.len - 1 - index2] = 0;
+                xSize = rl.measureText(text[0 .. text.len - index2 :0], 20);
+                index2 += 1;
+                if (xSize > SIZE) continue;
+                rl.drawText(text[0 .. text.len - index2 :0], offsetX + @mod(index, self.fileLayout) * SIZE + 3 + @divFloor(SIZE - xSize, 2), offsetY + topTextSize + 3 + @divFloor(index, self.fileLayout) * SIZE, 20, rl.Color.white);
+                break;
+            }
+        } else {
+            rl.drawText(f[if (self.path.len == 0) 0 else self.path.len + 1..], offsetX + @mod(index, self.fileLayout) * SIZE + 3 + @divFloor(SIZE - xSize, 2), offsetY + topTextSize + 3 + @divFloor(index, self.fileLayout) * SIZE, 20, rl.Color.white);
+        }
         index += 1;
     }
 
