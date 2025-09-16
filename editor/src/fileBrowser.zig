2a5aca86ae4e4c164cb53fbc544077b4a5764a88
Jacob Banta <jacob@banta.email>
2025-09-16 10:59:08 -0400

    editor filebrowser scrolling

--- editor/src/fileBrowser.zig
+++ editor/src/fileBrowser.zig
@@ -1,11 +1,13 @@
 const rl = @import("raylib");
 const std = @import("std");
-const SIZE = 150;
-files: [30]?[:0]const u8 = undefined,
-filePaths: [30]?[:0]const u8 = undefined,
+const MAX_FILE_COUNT = 1024;
+fileRenderSize: i32 = 150,
+files: [MAX_FILE_COUNT]?[:0]const u8 = .{null} ** MAX_FILE_COUNT,
+filePaths: [MAX_FILE_COUNT]?[:0]const u8 = .{null} ** MAX_FILE_COUNT,
 fileLayout: i32 = 0,
 path: [:0]const u8 = "",
 fileSelected: ?i32 = null,
+scroll: i32 = 1,
 
 size: struct {
     w: i32 = 0,
@@ -14,8 +16,8 @@ size: struct {
 
 pub fn init(self: *@This()) void {
     var walker = (std.fs.cwd().openDir("./", .{ .iterate = true }) catch unreachable).walk(std.heap.page_allocator) catch unreachable;
-    self.files = .{null} ** 30;
-    self.filePaths = .{null} ** 30;
+    self.files = .{null} ** MAX_FILE_COUNT;
+    self.filePaths = .{null} ** MAX_FILE_COUNT;
     while (walker.next() catch unreachable) |i| {
         if (std.mem.indexOf(u8, i.path, ".zig-cache") != null) continue;
         if (std.mem.indexOf(u8, i.path, "zig-out") != null) continue;
@@ -28,10 +30,11 @@ pub fn init(self: *@This()) void {
             break;
         }
     }
+    self.resizeEvent(self.size.w, self.size.h);
 }
 
 pub fn leftClickEvent(self: *@This(), x: i32, y: i32) void {
-    const fileSelected = @divFloor(x, SIZE) + @divFloor(y - topTextSize, SIZE) * self.fileLayout;
+    const fileSelected = @divFloor(x, self.fileRenderSize) + @divFloor(y - topTextSize, self.fileRenderSize) * self.fileLayout;
     if (fileSelected >= 0) {
         if (self.fileSelected) |f| {
             if (f == fileSelected) {
@@ -106,28 +109,39 @@ pub fn getPath(self: @This(), x: i32) ?i32 {
 pub fn resizeEvent(self: *@This(), w: i32, h: i32) void {
     self.size.w = w;
     self.size.h = h;
-    self.fileLayout = @intCast(@divFloor(w, SIZE));
+    self.fileRenderSize = h - topTextSize;
+    self.fileLayout = @intCast(@divFloor(w - scrollBarWidth, self.fileRenderSize));
+    var files: usize = 0;
+    for (self.files) |f| {
+        if (f == null) break;
+        files += 1;
+    }
+    while (self.scroll * self.fileLayout >= files) {
+        if (self.scroll == 0) break;
+        self.scroll -= 1;
+    }
 }
 
 const topTextSize = 30;
+const scrollBarWidth = 30;
 pub fn render_(self: @This(), offsetX: i32, offsetY: i32) void {
     rl.drawRectangle(offsetX, offsetY, self.size.w, self.size.h, rl.Color.dark_gray);
     var index: i32 = 0;
-    for (self.filePaths) |f| {
+    for (self.filePaths[@intCast(self.scroll * self.fileLayout)..@intCast(@min(((self.scroll + 1) * self.fileLayout), self.filePaths.len))]) |f| {
         if (std.mem.indexOf(u8, f orelse break, self.path)) |i| {
             if (i != 0) continue;
             if (f.?.len == self.path.len) continue;
         } else continue;
         if (std.mem.indexOf(u8, f.?[self.path.len + 1 ..], std.fs.path.sep_str) != null) continue;
-        rl.drawRectangle(2 + offsetX + @mod(index, self.fileLayout) * SIZE, 2 + offsetY + topTextSize + @divFloor(index, self.fileLayout) * SIZE, SIZE - 4, SIZE - 4, rl.Color.black);
+        rl.drawRectangle(2 + offsetX + index * self.fileRenderSize, 2 + offsetY + topTextSize, self.fileRenderSize - 4, self.fileRenderSize - 4, rl.Color.black);
         if (self.fileSelected) |i| {
-            if (index == i) {
-                rl.drawRectangleLines(1 + offsetX + @mod(index, self.fileLayout) * SIZE, 1 + offsetY + topTextSize + @divFloor(index, self.fileLayout) * SIZE, SIZE - 2, SIZE - 2, rl.Color.orange);
+            if (index + self.scroll * self.fileLayout == i) {
+                rl.drawRectangleLines(1 + offsetX + index * self.fileRenderSize, 1 + offsetY + topTextSize, self.fileRenderSize - 2, self.fileRenderSize - 2, rl.Color.orange);
             }
         }
         var xSize = rl.measureText(f.?[if (self.path.len == 0) 0 else (self.path.len + 1)..], 20);
 
-        if (xSize > SIZE) {
+        if (xSize > self.fileRenderSize) {
             const text = std.heap.page_allocator.dupeZ(u8, f.?[if (self.path.len == 0) 0 else self.path.len + 1..]) catch unreachable;
             defer std.heap.page_allocator.free(text);
             var index2: usize = 1;
@@ -139,12 +153,12 @@ pub fn render_(self: @This(), offsetX: i32, offsetY: i32) void {
                 text[text.len - 1 - index2] = 0;
                 xSize = rl.measureText(text[0 .. text.len - index2 :0], 20);
                 index2 += 1;
-                if (xSize > SIZE) continue;
-                rl.drawText(text[0 .. text.len - index2 :0], offsetX + @mod(index, self.fileLayout) * SIZE + 3 + @divFloor(SIZE - xSize, 2), offsetY + topTextSize + 3 + @divFloor(index, self.fileLayout) * SIZE, 20, rl.Color.white);
+                if (xSize > self.fileRenderSize) continue;
+                rl.drawText(text[0 .. text.len - index2 :0], offsetX + index * self.fileRenderSize + 3 + @divFloor(self.fileRenderSize - xSize, 2), offsetY + topTextSize + 3, 20, rl.Color.white);
                 break;
             }
         } else {
-            rl.drawText(f.?[if (self.path.len == 0) 0 else self.path.len + 1..], offsetX + @mod(index, self.fileLayout) * SIZE + 3 + @divFloor(SIZE - xSize, 2), offsetY + topTextSize + 3 + @divFloor(index, self.fileLayout) * SIZE, 20, rl.Color.white);
+            rl.drawText(f.?[if (self.path.len == 0) 0 else self.path.len + 1..], offsetX + index * self.fileRenderSize + 3 + @divFloor(self.fileRenderSize - xSize, 2), offsetY + topTextSize + 3, 20, rl.Color.white);
         }
         index += 1;
     }
